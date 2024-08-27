fdd29d2ed40cb333d7329fe81d1694efe33ac89d
smssarode123 <sagarsarode1997@gmail.com>
2024-08-27 12:51:09 +0530

    Exception package created

---
+++ AgentService/src/main/java/com/insurance/agent/exception/InvalidAgentIdException.java
@@ -0,0 +1,9 @@
+package com.insurance.agent.exception;
+
+public class InvalidAgentIdException extends RuntimeException{
+
+	public InvalidAgentIdException(String message) {
+		super(message);
+	}
+
+}
---
+++ AgentService/src/main/java/com/insurance/agent/exception/response/AgentIdHandler.java
@@ -0,0 +1,19 @@
+package com.insurance.agent.exception.response;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.insurance.agent.dto.ErrorResponse;
+import com.insurance.agent.exception.InvalidAgentIdException;
+
+public class AgentIdHandler {
+
+	public ResponseEntity<ErrorResponse> AgentIdErrorHandler(InvalidAgentIdException agent) 
+	{
+		ErrorResponse response = new ErrorResponse(agent.getMessage(), agent.getClass().getName(), new Date(), 404);
+		
+		return new ResponseEntity<ErrorResponse>(response, HttpStatus.NOT_FOUND);
+	}
+}
