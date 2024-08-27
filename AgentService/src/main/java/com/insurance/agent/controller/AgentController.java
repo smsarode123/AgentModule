1c9081d1caf9e92de84c78521835dacd96301b38
Lenovo <Lenovo@DESKTOP-C6G76QI>
2024-08-28 01:31:05 +0530

    merge Ashvini with Main

--- AgentService/src/main/java/com/insurance/agent/controller/AgentController.java
+++ AgentService/src/main/java/com/insurance/agent/controller/AgentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.insurance.agent.model.Agent;
@@ -15,7 +16,7 @@ public class AgentController {
 	
 	@Autowired
 	AgentService service;
-	
+	@GetMapping("/getAllAgents")
 	public ResponseEntity<List<Agent>> getAllAgents(){
 		List<Agent> list=service.getAllAgents();
 		
