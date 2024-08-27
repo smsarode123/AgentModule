8f206fdd454c6571875c1af85d6cbc60130f0ee6
Harish4216 <shindeharish229@gmail.com>
2024-08-27 14:00:00 +0530

    post api for agent

--- AgentService/src/main/java/com/insurance/agent/controller/AgentController.java
+++ AgentService/src/main/java/com/insurance/agent/controller/AgentController.java
@@ -1,8 +1,13 @@
 package com.insurance.agent.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.insurance.agent.model.Agent;
 import com.insurance.agent.service.AgentService;
 
 @RestController
@@ -10,6 +15,17 @@ public class AgentController {
 	
 	@Autowired
 	AgentService service;
+	
+	
+    @PostMapping("/saveagent")
+    public ResponseEntity<Agent>Postdata(@RequestBody Agent agent)
+    {
+    	
+    	Agent agentdata=service.saveagentdata(agent);
+    	
+    	return new ResponseEntity<Agent>(agentdata, HttpStatus.OK);
+    }
+	
 	
 
 	
--- AgentService/src/main/java/com/insurance/agent/serviceimpl/AgentServiceImpl.java
+++ AgentService/src/main/java/com/insurance/agent/serviceimpl/AgentServiceImpl.java
@@ -3,12 +3,23 @@ package com.insurance.agent.serviceimpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.insurance.agent.model.Agent;
 import com.insurance.agent.repository.AgentRepository;
 import com.insurance.agent.service.AgentService;
 
 @Service
-public class AgentServiceImpl implements AgentService{
+public class AgentServiceImpl implements AgentService {
 
-	@Autowired private AgentRepository repository;
+	@Autowired
+	private AgentRepository repository;
+
+	@Override
+	public Agent saveagentdata(Agent agent) {
+
+		Agent agentdata = repository.save(agent);
+
+		return agentdata;
+
+	}
 
 }
