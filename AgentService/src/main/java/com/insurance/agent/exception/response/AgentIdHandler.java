1592a91e2cac680012b9ad31ed768238b2dc2e0d
AshviniDahare <dahareashvini@gmail.com>
2024-08-29 12:59:15 +0530

    add ExcpetionHandler Annotation by Ashvini

--- AgentService/src/main/java/com/insurance/agent/exception/response/AgentIdHandler.java
+++ AgentService/src/main/java/com/insurance/agent/exception/response/AgentIdHandler.java
@@ -4,12 +4,16 @@ import java.util.Date;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.insurance.agent.dto.ErrorResponse;
 import com.insurance.agent.exception.InvalidAgentIdException;
-
+@RestControllerAdvice
 public class AgentIdHandler {
-
+	
+    @ExceptionHandler (InvalidAgentIdException.class)
 	public ResponseEntity<ErrorResponse> AgentIdErrorHandler(InvalidAgentIdException agent) 
 	{
 		ErrorResponse response = new ErrorResponse(agent.getMessage(), agent.getClass().getName(), new Date(), 404);
